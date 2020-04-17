b398339fc0473ec9b28cfdd91da9c58e66ecf467
runrunBilld <63849305+runrunBilld@users.noreply.github.com>
2020-04-17 21:32:22 +0800

    Revert "production v1.0"
    This reverts commit b4d443f7373e4023b7c061e0483151daed32217b.

--- web/js/pc1.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function() {
-	$("#Canon").animate({
-			width:'988px',
-			height:'10550px'
-		},1000,'swing'
-	);
-// 任何需要执行的js特效 
-//$("table tr:nth-child(even)").addClass("even"); 
-})
